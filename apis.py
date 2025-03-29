d8ee734883ea6f3870ad47d9cc00e8721fda2ee0
EgeAcar0 <egeacarr092@gmail.com>
2025-03-29 19:52:58 +0300

    Yeni değişiklikler

--- apis.py
+++ apis.py
@@ -2,3 +2,4 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 api_url = os.getenv('API_URL')
+api_key = os.getenv('API_KEY')
--- weather.py
+++ weather.py
@@ -1,9 +1,10 @@
 import requests
+from apis import api_key
 
 # Hava durumu fonksiyonu
 def get_weather(city="aydın"):
-    API_KEY = "0a6d1788ebffa57e1bf0eb71b6ea10fd"  # OpenWeatherMap
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang=tr&units=metric"
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&lang=tr&units=metric"
     response = requests.get(url)
     return response.json()
 
